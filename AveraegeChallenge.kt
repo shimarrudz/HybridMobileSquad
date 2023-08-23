12579a056ff4412e6d59ec30764397716b23515c
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-23 19:33:43 -0300

    feat: create average challenge

---
+++ AveraegeChallenge.kt
@@ -0,0 +1,9 @@
+fun main () {
+    var nota1: Double = 7.5
+    var nota2: Double = 8.0
+    var nota3: Double = 5.5
+
+    var media = (nota1 + nota2 + nota3)/3 
+
+    println("Valor da média: $media")
+}
