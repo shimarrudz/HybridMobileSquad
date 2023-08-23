c4b4d110d30b84b332caec284028e87345ac6db7
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-23 20:03:59 -0300

    create all challenges

---
+++ 02_Desafio_Temperatura/TempoChallenge.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var celsiu: Double = 25.0
+    var fahrenheit: Double = celsiu * 9/5 + 32
+    var kelvin: Double = celsiu + 273.15
+
+    println("A temperatura de celsiu é: $celsiu")
+    println("A temperatura de fahrenheit é: $fahrenheit")
+    println("A temperatura de kelvin é: $kelvin")
+
+}
---
+++ 03_DesafioPerfildoUsuario/UserProfileChallenge.kt
@@ -0,0 +1,10 @@
+fun main() {
+    
+val name: String = "Victor"
+val age: Int = 20
+val email: String = "vic.shima@example.com"
+var isActive: Boolean = false
+
+println("Meu nome é $name, tenho $age anos, meu email é $email, e estou ativo $isActive")
+
+}
